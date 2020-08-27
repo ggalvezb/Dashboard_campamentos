883b45d383348ed01f79ccdabf7ed5983ca913e2
Gonzalo Galvez <ggalvezb@live.com>
2020-08-26 20:26:19 -0400

    Updates

---
+++ precache-manifest.241539d9d5c093fab72cecca8e827a05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ad9e617211b40b97d0276119e65fb2",
+    "url": "/Dashboard_campamentos/index.html"
+  },
+  {
+    "revision": "684517d8412f790bdd1d",
+    "url": "/Dashboard_campamentos/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "b0cdc07ee5151b2fb0b8",
+    "url": "/Dashboard_campamentos/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "684517d8412f790bdd1d",
+    "url": "/Dashboard_campamentos/static/js/2.bd61fcfc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Dashboard_campamentos/static/js/2.bd61fcfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0cdc07ee5151b2fb0b8",
+    "url": "/Dashboard_campamentos/static/js/main.d8b9697e.chunk.js"
+  },
+  {
+    "revision": "68b3cccff4d1044be694",
+    "url": "/Dashboard_campamentos/static/js/runtime-main.3716dd95.js"
+  }
+]);
